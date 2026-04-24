8b315a2124bdc318b97ef58c2079943271379a50
Yani Yanev <Yani.Yanev@topicus.nl>
2026-04-24 15:18:08 +0200

    addeed test

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	for i := 0; i < 1; i++ {
 		notifyErr := bugsnag.Notify(err, metaData)
 		if notifyErr != nil {
-			fmt.Printf("Bugsnag SDK failed to send: %v\n", notifyErr)
+			fmt.Printf("Bugsnag SDK wddwwddwdwwdfailed to send: %v\n", notifyErr)
 			return
 		}
 	}
